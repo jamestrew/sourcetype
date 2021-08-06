5853fc06e71b8b1abbe8f77b181e1016feb5fc0c
mineugene <mineugene@users.noreply.github.com>
2021-08-06 16:52:11 -0400

    add keydown input event handler

--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -1,7 +1,18 @@
+import { KeyboardEvent } from "react";
+
+
 function Practice() {
+  const handleKeyDown = (
+    event: KeyboardEvent<HTMLInputElement>
+  ) => {
+    event.preventDefault();
+    console.log("detected keypress: ".concat(event.key))
+  }
+
   return (
     <main>
       <p>&laquo;main content&raquo;</p>
+      <input autoComplete="off" autoFocus onKeyDown={handleKeyDown} />
     </main>
   );
 }
