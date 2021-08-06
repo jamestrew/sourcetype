d7655ecd292147f3b0201efb70abb9a15f7a65ac
mineugene <mineugene@users.noreply.github.com>
2021-08-06 15:49:48 -0400

    add practice component Area in which user practises typing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/Header"
+import Practice from "./components/Practice"
 
 
 function App() {
   return (
     <>
       <Header />
+      <Practice />
     </>
   );
 }
---
+++ src/components/Practice.tsx
@@ -0,0 +1,9 @@
+function Practice() {
+  return (
+    <main>
+      <p>&laquo;main content&raquo;</p>
+    </main>
+  );
+}
+
+export default Practice;
