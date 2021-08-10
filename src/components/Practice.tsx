64f3a3b61f7fcc9dc8ce84afca39cbb0a12fcb75
James Trew <j.trew10@gmail.com>
2021-08-09 21:04:32 -0400

    add basic cursor movement in x

--- src/components/Practice.tsx
+++ src/components/Practice.tsx
@@ -1,11 +1,21 @@
-import { KeyboardEvent } from "react";
+import { KeyboardEvent, useState } from "react";
 import { Cursor } from "./Cursor";
 import { Letter, LetterState } from "./Letter";
 
 function Practice() {
+  const [cursorPos, setCursorPos] = useState({ x: 1, y: 25 });
+
   const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
     event.preventDefault();
+    // TODO: need to filter out more keys and probably want a better way
+    if (event.key === "Shift" || event.key === "Control") {
+      return;
+    }
     console.log("detected keypress: ".concat(event.key));
+
+    setCursorPos((cursorPos) => {
+      return { x: cursorPos.x + 7, y: cursorPos.y };
+    });
   };
 
   return (
@@ -14,7 +24,7 @@ function Practice() {
       <input autoComplete="off" autoFocus onKeyDown={handleKeyDown} />
       <div className="word-wrapper">
         {/* TODO: create a words class? */}
-        <Cursor hidden={false} xpad={12} ypad={25} />
+        <Cursor hidden={false} xpad={cursorPos.x} ypad={cursorPos.y} />
         <div className="words">
           <Letter letter="h" state={LetterState.correct} />
           <Letter letter="e" state={LetterState.correct} />
