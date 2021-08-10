a64e85ea60c8195cbf4dbbfd8783834a564f968a
mineugene <mineugene@users.noreply.github.com>
2021-08-09 20:59:38 -0400

    add wordlist component

---
+++ src/components/WordList.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Word } from "components/Word";
+
+interface IWordList {
+  text: string;
+}
+
+const WordList: React.FC<IWordList> = ({ text }) => {
+  return <></>;
+};
+
+export { WordList };
