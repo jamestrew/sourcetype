a6e2eedd8b90e77c42fcc2481b308fdc635329fd
mineugene <mineugene@users.noreply.github.com>
2021-08-10 17:54:50 -0400

    update children of wordlist with input

--- src/components/WordList.tsx
+++ src/components/WordList.tsx
@@ -1,12 +1,65 @@
-import React from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { Word } from "components/Word";
 
 interface IWordList {
-  text: string;
+  next?: string[];
+  children?: string;
 }
 
-const WordList: React.FC<IWordList> = ({ text }) => {
-  return <></>;
+interface IStateWordList {
+  wordElements: {
+    wordId: number;
+    letter: string;
+  }[];
+}
+
+const WordList: React.FC<IWordList> = ({ next, children }) => {
+  const [prog, setProg] = useState<IStateWordList["wordElements"]>([]);
+  const prevLength = useRef(next ? next.length : 0);
+  const tokenizedChild = children ? children.split(" ") : [];
+
+  const handleInput = useCallback(
+    (key: string) => {
+      if (key === "Backspace") {
+        prog.pop();
+      } else if (key === " ") {
+        prog.push({
+          wordId: prog[prog.length - 1].wordId + 1,
+          letter: key,
+        });
+      } else {
+        prog.push({
+          wordId: prog.length > 0 ? prog[prog.length - 1].wordId : 0,
+          letter: key,
+        });
+      }
+      setProg([...prog]);
+    },
+    [prog]
+  );
+
+  const fetchWord = useCallback(
+    (id: number) => {
+      return prog.filter((p) => p.wordId === id && p.letter !== " ");
+    },
+    [prog]
+  );
+
+  useEffect(() => {
+    if (!next || next.length === 0) return;
+    if (prevLength.current !== next.length) {
+      handleInput(next[next.length - 1]);
+      prevLength.current = next.length;
+    }
+  }, [next, handleInput]);
+
+  return (
+    <div className="WordList">
+      {tokenizedChild.map((wd, i) => (
+        <Word key={i} text={wd} value={fetchWord(i).map((i) => i.letter)} />
+      ))}
+    </div>
+  );
 };
 
 export { WordList };
