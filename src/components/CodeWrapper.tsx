8eaff4fe29d890a2920ac4730f18c200b82a238f
mineugene <mineugene@users.noreply.github.com>
2021-08-11 23:08:59 -0400

    move wordlist style within component

--- src/components/CodeWrapper.tsx
+++ src/components/CodeWrapper.tsx
@@ -48,9 +48,7 @@ export const CodeWrapper: FC<ICodeWrapper> = ({ codeBlock }) => {
         autoFocus
       />
       <Cursor hidden={false} xpad={cursorPos.x} ypad={cursorPos.y} />
-      <code className="w-4/5 py-8">
-        <WordList next={typed}>{codeBlock}</WordList>
-      </code>
+      <WordList next={typed}>{codeBlock}</WordList>
     </main>
   );
 };
