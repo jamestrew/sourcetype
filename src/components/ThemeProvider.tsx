8c2ec72fd7175f0b63ca1a1b5446ceab39dd3649
James Trew <j.trew10@gmail.com>
2021-08-15 22:26:56 -0400

    add dark theme and theme switcher
    cuz MY EYES WERE BURNING

---
+++ src/components/ThemeProvider.tsx
@@ -0,0 +1,26 @@
+// ThemeProvider.js
+
+import { FC, useState } from "react";
+
+export const ThemeProvider: FC = ({ children }) => {
+  const [theme, setTheme] = useState(`dark`);
+
+  return (
+    <div
+      id="theme-wrapper"
+      className={`bg-primary text-primary ${theme} h-screen`}
+    >
+      {children}
+
+      <div className="grid justify-items-center">
+        <button
+          onClick={() => {
+            theme == `light` ? setTheme(`dark`) : setTheme(`light`);
+          }}
+        >
+          Theme: {theme}
+        </button>
+      </div>
+    </div>
+  );
+};
