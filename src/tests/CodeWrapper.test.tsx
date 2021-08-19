2511011d522840a3e0987cecf0ba450f4ec30ef3
mineugene <mineugene@users.noreply.github.com>
2021-08-19 18:59:13 -0400

    clean-up unused imports

--- src/tests/CodeWrapper.test.tsx
+++ src/tests/CodeWrapper.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, act } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { CodeWrapper, Typed, testing } from "components/CodeWrapper";
 import "@testing-library/jest-dom/extend-expect";
