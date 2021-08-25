2840a2e1a7030c314e248544ea0bccb517afc176
James Trew <j.trew10@gmail.com>
2021-08-24 21:34:57 -0400

    build: bootstrap apollo-server-express

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,36 @@
+import { ApolloServer } from "apollo-server-express";
+import express = require("express");
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { User } from "./entity/User";
+// import { User } from "./entity/User";
+import { buildTypeDefsAndResolvers } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
-createConnection()
-  .then(async (connection) => {
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+const main = async () => {
+  const connection = await createConnection();
+  const app = express();
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+  const { typeDefs, resolvers } = await buildTypeDefsAndResolvers({
+    resolvers: [HelloResolver],
+  });
 
-    console.log("Here you can setup and run express/koa/any other framework.");
-  })
-  .catch((error) => console.log(error));
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+  app.get("/", (_, res) => {
+    res.send("hello");
+  });
+
+  app.listen(4000, () => {
+    console.log("server started on localhost:4000");
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
