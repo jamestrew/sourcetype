3bf3d00a638527ddd278f28fd8c18b79b3473959
James Trew <j.trew10@gmail.com>
2021-08-30 21:26:44 -0400

    clean out all sourcegraph components
    maybe just for now. closes #27 #28

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,7 @@
-import {
-  ApolloClient,
-  createHttpLink,
-  gql,
-  InMemoryCache,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
 import { ThemeProvider } from "components/ThemeProvider";
 import Header from "./components/Header";
 import { Practice } from "./components/Practice";
-
-const httpLink = createHttpLink({
-  uri: "https://sourcegraph.com/.api/graphql",
-  fetchOptions: {
-    mode: "no-cors",
-  },
-});
-
-const authLink = setContext((_, { headers }) => {
-  const token = process.env.SRCGRAPH_TOKEN;
-  return {
-    headers: {
-      ...headers,
-      "Access-Control-Allow-Origin": "*",
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
-});
-
-const srcgraphClient = new ApolloClient({
-  link: authLink.concat(httpLink),
-  credentials: "include",
-  cache: new InMemoryCache(),
-});
-
-srcgraphClient
-  .query({
-    query: gql`
-      query testquery($first: Int!) {
-        repositories(first: $first) {
-          nodes {
-            name
-            description
-            url
-          }
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
 
 const client = new ApolloClient({
   uri: "https://48p1r2roz4.sse.codesandbox.io",
