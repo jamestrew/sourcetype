7813c9fadbb051e09bc7b1b8592250dc291fa885
James Trew <j.trew10@gmail.com>
2021-09-18 21:52:28 -0400

    feat: add space and letter start test

--- src/tests/CodeWrapper/keyHandler.test.ts
+++ src/tests/CodeWrapper/keyHandler.test.ts
@@ -1,10 +1,13 @@
+import {
+  curXStart,
+  curXStep,
+  curYStart,
+} from "components/CodeWrapper/CodeWrapper";
 import { TAB } from "utils/constants";
 import createKeyHandler, {
   testing,
 } from "../../components/CodeWrapper/keyHandler";
 const { KeyHandler, BackspaceHandler, EnterHandler, SpaceHandler } = testing;
-
-const foo = new KeyHandler({});
 
 const sCode = [
   ["if", "(true)", "{"],
@@ -14,4 +17,65 @@ const sCode = [
   ["}"],
 ];
 
-const bCode = [];
+// prettier-ignore
+const bCode = [
+  "if", "(true)", "{",
+  "", "", "if", "(bar)", "{",
+  "", "", "", "", "return", "'foo'",
+  "", "", "}",
+  "}",
+];
+
+describe("LETTERS", () => {
+  it("letter", () => {
+    const typed = {
+      currentWordId: 0,
+      current: [],
+    };
+    const cursorPos = {
+      x: curXStart,
+      y: curYStart,
+    };
+    const handler = new KeyHandler({
+      key: "a",
+      typed,
+      cursorPos,
+      sSplit: sCode,
+      bSplit: bCode,
+      tabSize: 2,
+    });
+
+    handler.handleKey();
+    expect(handler.newTyped).toEqual({
+      currentWordId: 0,
+      current: [{ wordId: 0, letter: "a" }],
+    });
+    expect(handler.newCursorPos).toEqual({
+      x: curXStart + curXStep,
+      y: curYStart,
+    });
+  });
+});
+
+describe("SPACE", () => {
+  it("at start", () => {
+    const typed = {
+      currentWordId: 0,
+      current: [],
+    };
+    const cursorPos = {
+      x: curXStart,
+      y: curYStart,
+    };
+    const handler = new SpaceHandler({
+      key: " ",
+      typed,
+      cursorPos,
+      sSplit: sCode,
+      bSplit: bCode,
+      tabSize: 2,
+    });
+
+    expect(handler.ignoreInput()).toBe(true);
+  });
+});
