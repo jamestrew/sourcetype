3843b213846abad110cdaf47e4b2fa6527f1897e
James Trew <j.trew10@gmail.com>
2021-10-04 20:41:23 -0400

    fix: reset cursorPos and typed upon new code

--- src/components/CodeWrapper/CodeWrapper.tsx
+++ src/components/CodeWrapper/CodeWrapper.tsx
@@ -1,4 +1,4 @@
-import { KeyboardEvent, FC, useState, useRef } from "react";
+import { KeyboardEvent, FC, useState, useRef, useEffect } from "react";
 import { Cursor } from "../Cursor";
 import { Word } from "../Word";
 import { Tab } from "../Tab";
@@ -31,6 +31,15 @@ export const CodeWrapper: FC<ICodeWrapper> = ({ sSplitCode, bSplitCode }) => {
   const blurCodeRef = useRef<HTMLParagraphElement>(null);
   const [focusWarning, setFocusWarning] = useState<FocusWarning>("hidden");
   const [blurred, setBlurred] = useState<Blurred>("");
+
+  const clearCurTyped = () => {
+    setCursorPos({ x: curXStart, y: curYStart });
+    setTyped({ currentWordId: 0, current: [] });
+  };
+
+  useEffect(() => {
+    clearCurTyped();
+  }, [sSplitCode, bSplitCode]);
 
   const handleKeyPress = (event: KeyboardEvent<HTMLInputElement>) => {
     event.preventDefault();
