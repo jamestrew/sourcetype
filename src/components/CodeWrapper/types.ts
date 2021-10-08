dae1b6d95d8f504cafed57dddc5a0f2c3bd8c59f
James Trew <66286082+trewjames@users.noreply.github.com>
2021-10-07 23:03:33 -0400

    Endgame hide (#58)
    * squash! create temporary endgame screen div
    
    * feat: add basic round over feedback overlay
    
    * squash! hide background

--- src/components/CodeWrapper/types.ts
+++ src/components/CodeWrapper/types.ts
@@ -33,5 +33,5 @@ export type CursorPos = {
   y: number;
 };
 
-export type FocusWarning = "hidden" | "";
-export type Blurred = "blurred" | "";
+export type Hidden = "hidden" | "";
+export type WrapClass = "blurred" | "endscreen" | "";
