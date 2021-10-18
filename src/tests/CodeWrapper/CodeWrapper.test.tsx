1f2b58652d91aecf43c8a4689e1702df74606e8e
James Trew <j.trew10@gmail.com>
2021-10-18 17:45:36 -0400

    chore: clean up test for deprecated code

--- src/tests/CodeWrapper/CodeWrapper.test.tsx
+++ src/tests/CodeWrapper/CodeWrapper.test.tsx
@@ -1,15 +1,11 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { CodeWrapper, testing } from "../../components/CodeWrapper/CodeWrapper";
+import { CodeWrapper } from "../../components/CodeWrapper/CodeWrapper";
 import "@testing-library/jest-dom/extend-expect";
-import { ENTER_CODE } from "utils/constants";
-const { currentTypedWord } = testing;
 
 describe("CodeWrapper", () => {
   beforeEach(() => {
-    render(
-      <CodeWrapper sSplitCode={[["foo"]]} bSplitCode={["foo"]} tabSize={2} />
-    );
+    render(<CodeWrapper sSplitCode={[["foo"]]} bSplitCode={["foo"]} />);
     jest.useFakeTimers();
   });
 
@@ -43,35 +39,5 @@ describe("CodeWrapper", () => {
       },
       { interval: 100, timeout: 1500 }
     );
-  });
-});
-
-describe("currentTypedWord", () => {
-  it("basic", () => {
-    const typed = {
-      currentWordId: 0,
-      current: [
-        { wordId: 0, letter: "a" },
-        { wordId: 0, letter: "b" },
-        { wordId: 0, letter: "c" },
-      ],
-    };
-    expect(currentTypedWord(typed, 0)).toEqual(["a", "b", "c"]);
-  });
-
-  it("new line word", () => {
-    const typed = {
-      currentWordId: 0,
-      current: [
-        { wordId: 0, letter: "a" },
-        { wordId: 0, letter: "b" },
-        { wordId: 0, letter: "c" },
-        { wordId: 1, letter: ENTER_CODE },
-        { wordId: 1, letter: "d" },
-        { wordId: 1, letter: "e" },
-        { wordId: 1, letter: "f" },
-      ],
-    };
-    expect(currentTypedWord(typed, 1)).toEqual(["d", "e", "f"]);
   });
 });
